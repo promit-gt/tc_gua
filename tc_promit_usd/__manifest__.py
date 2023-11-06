957593ac2d6630ecf63fb6ff1dfd3250b860ba60
PromitINC <47229465+kevturpromit@users.noreply.github.com>
2023-11-06 11:26:40 -0600

    upgrade

--- tc_promit_usd/__manifest__.py
+++ tc_promit_usd/__manifest__.py
@@ -1,10 +1,12 @@
 # -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
 {
     'name': "Tipo de Cambio Guatemala USD",
 
     'summary': """Modulo que actualiza el tipo de cambio diario en base a BANGUAT, solo funciona para USD
     Configure la hora de actualización en Técnico>>Acciones Planificadas>>Enviar Solicitud de Tipo de Cambio
     """,
+    "license": "OPL-1",
 
     'description': """
         Modulo que actualiza el tipo de cambio diario en base a BANGUAT"
@@ -15,6 +17,7 @@
 
     'category': 'Uncategorized',
     'version': '0.1',
+    'images': ['static/description/logo.jpeg'],
 
     'depends': ['account'],
 
